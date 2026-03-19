9d04f955b78f562d00716d7d04a13bb00de4fd8e
lucas videla <lucas.videla994@gmail.com>
2026-03-19 02:32:56 -0300

    hotfix

--- src/routes/tasks/index.ts
+++ src/routes/tasks/index.ts
@@ -3,7 +3,7 @@ import { projects } from './projects';
 import { taskManager, tasks } from './routes';
 import { schedules } from './schedules';
 
-const tasksGroup = new Hono().route('/tasks', tasks).route('/schedules', schedules).route('/projects', projects);
+const tasksGroup = new Hono().route('/', tasks).route('/schedules', schedules).route('/projects', projects);
 
 export { tasksGroup, taskManager };
 export type TasksGroup = typeof tasksGroup;
